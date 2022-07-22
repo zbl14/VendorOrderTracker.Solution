fc5a29f5cf251dd94f8d9a3c202d87f8cb42059b
zbl14 <ifthereisoneday@gmail.com>
2022-07-22 14:26:37 -0700

    add ID property for Vendor Contructure

--- VendorOrderTracker/Models/Vender.cs
+++ VendorOrderTracker/Models/Vender.cs
@@ -6,6 +6,7 @@ namespace VendorOrderTracker.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
+    public int Id { get; }
     public List<Order> Orders { get; set; }
     private static List<Vendor> _instances = new List<Vendor> {};
 
@@ -14,6 +15,7 @@ namespace VendorOrderTracker.Models
       Name = name;
       Orders = new List<Order> {};
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public Vendor (string name, string description) : this (name)
