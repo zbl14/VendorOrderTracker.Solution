19427d429e01f6e4d73d83de9f9b17dcce9bff58
zbl14 <ifthereisoneday@gmail.com>
2022-07-22 14:27:58 -0700

    add Show(), show a vendor's detail

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -1,6 +1,7 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using VendorOrderTracker.Models;
-using System.Collections.Generic;
 
 namespace VendorOrderTracker.Controllers
 {
@@ -24,6 +25,17 @@ namespace VendorOrderTracker.Controllers
     {
       Vendor newVendor = new Vendor(vendorName);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
     }
   }
 }
