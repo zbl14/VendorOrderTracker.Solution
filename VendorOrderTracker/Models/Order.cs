48b210ca3323fd1d5b25c5f73b584787561b0b9f
zbl14 <ifthereisoneday@gmail.com>
2022-07-22 09:50:20 -0700

    get price, test passed

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -2,14 +2,19 @@ namespace VendorOrderTracker.Models
 {
   public class Order
   {
+    public string Name { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
     public int Price { get; set; }
     public string Date { get; set; }
 
-    public Order (string Title, string Description, int Price, string Date)
+    public Order (string name, string title, string description, int price, string date)
     {
-
+      Name = name;
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
     }
   }
 }
