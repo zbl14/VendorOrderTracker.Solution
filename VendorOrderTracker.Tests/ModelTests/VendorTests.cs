efbeb63807b742fe131ef4eb9833000a882bd34b
zbl14 <ifthereisoneday@gmail.com>
2022-07-22 09:40:37 -0700

    comment out unnecessary code

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -12,11 +12,11 @@ namespace VendorOrderTracker.Tests
     //   ClassName.ClearAll();
     // }
 
-    [TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-    {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-    }
+    // [TestMethod]
+    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+    //   // any necessary logic to prep for test; instantiating new classes, etc.
+    //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    // }
   }
 }
