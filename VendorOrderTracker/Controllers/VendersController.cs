0154ed40fd05d4513430b2c327422d97b7cce6e8
zbl14 <ifthereisoneday@gmail.com>
2022-07-22 11:33:18 -0700

    add new route

--- VendorOrderTracker/Controllers/VendersController.cs
+++ VendorOrderTracker/Controllers/VendersController.cs
@@ -12,5 +12,11 @@ namespace VendorOrderTracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
   }
 }
